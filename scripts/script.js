8b85ab70ea7e93242213fe69dc2b8f628b090d6d
German Ayala <41121887+whoisgerman@users.noreply.github.com>
2025-12-03 00:28:00 -0500

    mobile optimization

--- scripts/script.js
+++ scripts/script.js
@@ -16,3 +16,43 @@ function typeWriter() {
 window.onload = typeWriter;
 
 //Light / Dark modes
+
+
+//Mobile navbar and its accessibility
+const openButton = document.getElementById('open-sidebar-button');
+const navbar = document.getElementById('navbar');
+
+const media = window.matchMedia("(width < 900px)");
+
+media.addEventListener('change', (e) => updateNavbar())
+
+function updateNavbar(e) {
+  const isMobile = e.matches
+  console.log(isMobile);
+  if(isMobile) {
+    navbar.setAttribute('inert','');
+  }
+  else{
+    //desktop device
+    navbar.removeAttribute('inert');
+  }
+}
+
+function openSidebar() {
+  navbar.classList.add('show');
+  openButton.setAttribute('aria-expanded', 'true');
+  navbar.removeAttribute('inert');
+}
+
+function closeSidebar() {
+  navbar.classList.remove('show');
+  openButton.setAttribute('aria-expanded', 'false');
+  navbar.setAttribute('inert','');
+}
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link => {
+  link.addEventListener('click', closeSidebar);
+});
+
+updateNavbar(media);
